eb6c9acada66e79ccc60708de2bd4589bef623ab
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-05-30 18:29:13 +0200

    stored useful apis in api.rb

---
+++ lib/api.rb
@@ -0,0 +1,17 @@
+require "uri"
+require "net/http"
+require 'json'
+
+# Magic Eden: Provides info about a wallet and the NFTs it holds
+# Name, collection and image
+# url = URI("https://api-mainnet.magiceden.dev/v2/wallets/#{public_wallet_key}")
+
+# Floor price: Pass in collection
+url = URI("https://api-mainnet.magiceden.dev/v2/collections/#{collection}/stats")
+
+
+http = Net::HTTP.new(url.hostname, url.port);
+request = Net::HTTP::Get.new(url)
+http.use_ssl = true
+response = http.request(request)
+p result = JSON.parse(response.body)
