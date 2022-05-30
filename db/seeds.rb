6ec6ace8ed3213173151a0f97ca58dff9042f00e
Spiros Tsiakalos <spiros.tsiakalos1@gmail.com>
2022-05-30 21:40:12 +0300

    created the seeds for solana history

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_solana = File.read(Rails.root.join('db/solana_prices.csv'))
+solana_history = CSV.parse(csv_solana, headers: true, encoding: 'ISO-8859-1')
+
+solana_history.each do |row|
+  SolanaTimestamp.create(
+    date: row['timestamp'],
+    price: row['high']
+  )
+end
