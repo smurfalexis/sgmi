981eb308a13c8d87820aefe478647a964a76efc3
raisatarase <99401852+raisatarase@users.noreply.github.com>
2022-06-01 11:18:17 +0200

    sodekiq added

---
+++ app/jobs/daily_solana_job.rb
@@ -0,0 +1,7 @@
+class DailySolanaJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/daily_solana_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DailySolanaJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
