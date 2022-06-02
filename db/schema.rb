adb51a0a83a7b68eca58cc6703daa10fad665128
Mahmoud Hajji <rami_hajji@outlook.de>
2022-06-02 12:20:33 +0200

    added nfts, wallets and updated collections in the seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_084139) do
+ActiveRecord::Schema.define(version: 2022_06_02_093427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2022_06_02_084139) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "purchase_date"
+    t.string "mint_address"
     t.index ["collection_id"], name: "index_nfts_on_collection_id"
     t.index ["wallet_id"], name: "index_nfts_on_wallet_id"
   end
