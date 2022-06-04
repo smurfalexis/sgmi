9092aa97a48a9f8bc52abe12c849676033fa6c73
Spiros Tsiakalos <spiros.tsiakalos1@gmail.com>
2022-06-04 20:26:36 +0200

    watchlist creation on user sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :create_watchlist
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +7,14 @@ class User < ApplicationRecord
   has_many :wallets
   has_many :nfts, through: :wallets
   has_one :watchlist
+
+
+  private
+
+
+  def create_watchlist
+    @user = User.last
+    @watchlist = Watchlist.create(user: @user)
+    @user.save
+  end
 end
