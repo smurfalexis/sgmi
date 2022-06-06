89fc4610a0482c893e1debd09c790ac3fbe66a0d
Mahmoud Hajji <rami_hajji@outlook.de>
2022-06-06 14:51:55 +0200

    update

---
+++ app/controllers/wallets_controller.rb
@@ -0,0 +1,16 @@
+class WalletsController < ApplicationController
+
+
+def new
+end 
+
+def create
+end
+
+def 
+
+
+
+
+
+end
--- lib/launchpad_collections.rb
+++ lib/launchpad_collections.rb
@@ -4,30 +4,39 @@ require 'json'
 
 
 def get_all_collections
-offset = 0
-limit = 500
-result = collection_by_offset(offset, limit)
-  while result.length == limit
-    save_collections(result)
-    offset = offset + limit
-    result = collection_by_offset(offset, limit)
+  offset = 0
+  limit = 500
+  result = collection_by_offset
+    while result.length == limit
+      p result.length
+      sleep 5
+      puts "slept for 5 sec, calling api again"
+      # save_collections(result)
+      offset = offset + limit
+      result = collection_by_offset
+      p offset
+      p limit
+    end
+  #  save_collections(result)
   end
-  save_collections(result)
-end
 
-def collection_by_offset(offset, limit)
-  url = URI("api-devnet.magiceden.dev/v2/collections?offset=#{offset}&limit=#{limit}")
-  http = Net::HTTP.new(url.hostname, url.port)
-  request = Net::HTTP::Get.new(url)
-  http.use_ssl = true
-  response = http.request(request)
-  result = JSON.parse(response.body)
-end
-
-def save_collections(result)
-  result.each do |collection|
-    Collection.create(symbol: result['symbol'], name: result['name'], description: result['description']
-                      image: result['image'], twitter: result['twitter'], discord: result['discord'],
-                      categories: result['categories'].join)
+  def collection_by_offset(offset, limit)
+    url = URI("https://api-mainnet.magiceden.dev/v2/collections?offset=#{offset}&limit=#{limit}")
+    http = Net::HTTP.new(url.hostname, url.port)
+    request = Net::HTTP::Get.new(url)
+    http.use_ssl = true
+    response = http.request(request)
+    result = JSON.parse(response.body)
+    result.length
   end
-end
+
+  # def save_collections(result)
+  #   result.each do |r|
+  #     Collection.create(symbol: r['symbol'], name: r['name'], description: r['description'],
+  #                       image: r['image'], twitter: r['twitter'], discord: r['discord'],
+  #                       category: r['categories'].join)
+  #   end
+  # end
+
+
+p collection_by_offset()
