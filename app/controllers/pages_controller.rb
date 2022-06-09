e45c1bf37b4dc07262f1a46e8c313c7f10154a6f
Mahmoud Hajji <rami_hajji@outlook.de>
2022-06-09 09:56:50 +0200

    updated pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -48,61 +48,67 @@ class PagesController < ApplicationController
     @wallet = Wallet.where(user: current_user)
     @nfts = Nft.where(wallet: @wallet)
     @user = current_user
-    @data_keys = { '12.04.2022' => 5,
-                   '19.04.2022' => 7,
-                   '26.04.2022' => 10,
-                   '03.05.2022' => 25,
-                   '10.05.2022' => 36,
-                   '17.05.2022' => 47,
-                   '24.05.2022' => 33,
-                   '31.05.2022' => 110 }
+  end 
 
-    @okay = Collection.find_by(name: 'Okay Bears')
-    @okay.floor_price = @data_keys
+  #   @data_keys = { '12.04.2022' => 5,
+  #                  '19.04.2022' => 7,
+  #                  '26.04.2022' => 10,
+  #                  '03.05.2022' => 25,
+  #                  '10.05.2022' => 36,
+  #                  '17.05.2022' => 47,
+  #                  '24.05.2022' => 33,
+  #                  '31.05.2022' => 110 }
 
-    @data_values = [100, 400, 175, 200, 50, 350, 600]
+  #   @okay = Collection.find_by(name: 'Okay Bears')
+  #   # @okay.floor_price = @data_keys
 
-    @degods_fp = { '12.04.2022' => 8,
-                   '19.04.2022' => 10,
-                   '26.04.2022' => 14,
-                   '03.05.2022' => 23,
-                   '10.05.2022' => 29,
-                   '17.05.2022' => 55,
-                   '24.05.2022' => 33,
-                   '31.05.2022' => 110 }
-    @degods = Collection.find_by(name: 'DeGods')
-    @degods.floor_price = @degods_fp
-    @smokeheads = Collection.find_by(name: 'Smoke Heads')
-    @smokeheads_fp = { '12.04.2022' => 5,
-                       '19.04.2022' => 29,
-                       '26.04.2022' => 90,
-                       '03.05.2022' => 25,
-                       '10.05.2022' => 16,
-                       '17.05.2022' => 47,
-                       '24.05.2022' => 53,
-                       '31.05.2022' => 193 }
-    @smokeheads.floor_price = @smokeheads_fp
-    @cardboard = Collection.find_by(name: 'Cardboard Citizens')
-    @cardboard_fp = { '12.04.2022' => 5,
-                      '19.04.2022' => 7,
-                      '26.04.2022' => 10,
-                      '03.05.2022' => 5,
-                      '10.05.2022' => 26,
-                      '17.05.2022' => 97,
-                      '24.05.2022' => 32,
-                      '31.05.2022' => 300 }
-    @cardboard.floor_price = @cardboard_fp
-    @nfts_chart = []
-    @nfts_chart << @okay
-    @nfts_chart << @degods
-    @nfts_chart << @smokeheads
-    @nfts_chart << @cardboard
-    @collections = Collection.all
-  end
+  #   @data_values = [100, 400, 175, 200, 50, 350, 600]
+
+  #   @degods_fp = { '12.04.2022' => 8,
+  #                  '19.04.2022' => 10,
+  #                  '26.04.2022' => 14,
+  #                  '03.05.2022' => 23,
+  #                  '10.05.2022' => 29,
+  #                  '17.05.2022' => 55,
+  #                  '24.05.2022' => 33,
+  #                  '31.05.2022' => 110 }
+
+  #   @degods = Collection.find_by(name: 'DeGods')
+  #   @degods.floor_price = @degods_fp
+  #   @smokeheads = Collection.find_by(name: 'Smoke Heads')
+  #   @smokeheads_fp = { '12.04.2022' => 5,
+  #                      '19.04.2022' => 29,
+  #                      '26.04.2022' => 90,
+  #                      '03.05.2022' => 25,
+  #                      '10.05.2022' => 16,
+  #                      '17.05.2022' => 47,
+  #                      '24.05.2022' => 53,
+  #                      '31.05.2022' => 193 }
+  #   @smokeheads.floor_price = @smokeheads_fp
+  #   @cardboard = Collection.find_by(name: 'Cardboard Citizens')
+  #   @cardboard_fp = { '12.04.2022' => 5,
+  #                     '19.04.2022' => 7,
+  #                     '26.04.2022' => 10,
+  #                     '03.05.2022' => 5,
+  #                     '10.05.2022' => 26,
+  #                     '17.05.2022' => 97,
+  #                     '24.05.2022' => 32,
+  #                     '31.05.2022' => 300 }
+  #   @cardboard.floor_price = @cardboard_fp
+  #   @nfts_chart = []
+  #   @nfts_chart << @okay
+  #   @nfts_chart << @degods
+  #   @nfts_chart << @smokeheads
+  #   @nfts_chart << @cardboard
+  #   @collections = Collection.all
+  # end
 
   def about; end
 
   def introduction
+  end
+  
+  def contact
   end
 
   # Best performing NFTs
